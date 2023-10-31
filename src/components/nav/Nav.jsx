179dce8800c22634e375ed8c81c2d1bd7ae04757
brent-thomas <brent@itsnotabugitsafeature.com>
2023-10-30 21:28:58 -0400

    finished connect page and add styles to navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,16 +3,16 @@ import logo from '../../assets/logo.png';
 import styles from './nav.module.css';
 import { Link, useMatch } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faChevronRight, faTimes } from '@fortawesome/free-solid-svg-icons'
 const Nav = () => {
   const isActive = (path) => !!useMatch(path);
   const [showMobileDropDown, setshowMobileDropDown] = useState(false)
 
   return (
-    <div className={`${styles.container} pd-vt pd-hz`}>
-      <div className={styles.mobile_navigation}>
-        {/*//Mobile Navigaiton Bar//*/}
-        <div className={styles.mobileNavBar}>
+    <div className={styles.container}>
+      <div className={`${styles.mobile_navigation} pd-hz pd-vt`}>
+        {/*//Mobile Navigation Bar//*/}
+        <div className={`${styles.mobileNavBar}`}>
           <Link to="/">
             <img src={logo} className={styles.logo} />
           </Link>
@@ -26,20 +26,48 @@ const Nav = () => {
           </button>
         </div>
          {/*//Mobile Dropdown Menu//*/}
-        <div className={`${styles.mobileDropDown} pd-hz pd-vt`} 
-        style={showMobileDropDown ? {right:'0'} : {right:'-100%'}}
-        >
-          <Link to="/" className={isActive('/') ? styles.activeLink : ''}>
-          Blog</Link>
-          <Link to="/javascript" className={isActive('/javascript') ? styles.activeLink : ''}>Javascript</Link>
-          <Link to="/python" className={isActive('/python') ? styles.activeLink : ''}>Python</Link>
-          <Link to="/education" className={isActive('/education') ? styles.activeLink : ''}>Education</Link>
-          <Link to="/connect" className={isActive('/connect') ? styles.activeLink : ''}>Connect</Link>
+         <div className={`${styles.mobileDropDown}`} 
+         style={showMobileDropDown ? {transform: 'translateX(0)'} : {transform: 'translateX(100%)'}} >
+
+          <Link to="/" className='pd-hz pd-vt' onClick={()=> setshowMobileDropDown(false)}>
+              <div className={styles.menuItem}>
+                  <p className={isActive('/') ? styles.activeLink : ''}>Blog</p>
+                  <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+          </Link>
+
+          <Link to="/javascript" className='pd-hz pd-vt' onClick={()=> setshowMobileDropDown(false)}>
+            <div className={styles.menuItem}>
+                <p className={isActive('/javascript') ? styles.activeLink : ''}>Javascript</p>
+                <FontAwesomeIcon icon={faChevronRight}/>
+            </div>
+          </Link>
+
+          <Link to="/python" className='pd-hz pd-vt' onClick={()=> setshowMobileDropDown(false)}>
+              <div className={styles.menuItem}>
+                  <p className={isActive('/python') ? styles.activeLink : ''}>Python</p>
+                  <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+          </Link>
+
+          <Link to="/education" className='pd-hz pd-vt' onClick={()=> setshowMobileDropDown(false)}>
+              <div className={styles.menuItem}>
+                  <p className={isActive('/education') ? styles.activeLink : ''}>Education</p>
+                  <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+          </Link>
+
+          <Link to="/connect" className='pd-hz pd-vt' onClick={()=> setshowMobileDropDown(false)}>
+              <div className={styles.menuItem}>
+                  <p className={isActive('/connect') ? styles.activeLink : ''}>Connect</p>
+                  <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+          </Link>
         </div>
       </div>
 
       {/*/Desktop Navigaiton//*/}
-      <div className={styles.desktopNavigation}>
+      <div className={`${styles.desktopNavigation} pd-hz pd-vt`}>
         <Link to="/">
           <img src={logo} className={styles.logo} />
         </Link>
--- src/pages/connect/Connect.jsx
+++ src/pages/connect/Connect.jsx
@@ -1,8 +1,39 @@
+import styles from './connect.module.css'
+import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faChevronRight, faEnvelope, faFrog, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faHackerrank, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Connect = () => {
   return (
-    <div>
-        
+    <div className={styles.container}>
+         {/*/Social Links//*/}
+            <h2>Connect With Me</h2>
+            <div>
+            <Link target="_blank" to="https://www.linkedin.com/in/ken1098/" className={styles.socialLink}>
+                <FontAwesomeIcon icon={faLinkedin} className={styles.icon} id={styles.linkedin}/>
+                <p>LinkedIn</p>
+            </Link>
+
+            <Link target="_blank" to="https://github.com/brent-thomas" className={styles.socialLink}>
+                <FontAwesomeIcon icon={faGithub} className={styles.icon}/>
+                <p>GitHub</p>
+            </Link>
+
+            <Link target="_blank" to="https://www.hackerrank.com/profile/brent48" className={styles.socialLink}>
+                <FontAwesomeIcon icon={faHackerrank} className={styles.icon} id={styles.hackerrank}/>
+                <p>HackerRank</p>
+            </Link>
+
+            <Link target="_blank" to="https://teamtreehouse.com/profiles/brentthomas4" className={styles.socialLink}>
+                <FontAwesomeIcon icon={faFrog} className={styles.icon} id={styles.treehouse}/>
+                <p>Treehouse</p>
+            </Link>
+            <a href="mailto:brent@itsnotabugitsafeature.com?subject=Job Offer&body=Hello Brent! We'd love to offer you a position as a (insert job title) at (insert company name), with an annual salary of (all the monies 😉). When can you start?" className={styles.socialLink}>
+                <FontAwesomeIcon icon={faEnvelope} className={styles.icon} id={styles.email}/>
+                <p>Email</p>
+            </a>
+        </div>
     </div>
   )
 }
