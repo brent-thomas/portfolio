1e69976ff9653b8b6e0b03187c35c508e196b9ce
brent-thomas <brent@itsnotabugitsafeature.com>
2024-08-18 22:46:13 -0400

    added zap project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/home/Home';
 import './App.css'
@@ -7,27 +7,68 @@ import Connect from './pages/connect/Connect';
 import Education from './pages/education/Education';
 import Python from './pages/python/Python';
 import Javascript from './pages/javascript/Javascript';
-import { useEffect } from 'react';
 import AntiTherapy from './pages/javascript/projects/antitherapy/AntiTherapy';
+import Typewriter from 'typewriter-effect';
+import doorKnock from './assets/door_bang.mp3'
+
 
 function ConditionalNav() {
   const location = useLocation();
-
   if (location.pathname.includes('/projects')) {
-      return null; // Don't render Nav if the URL includes '/projects'
+      return null;
   }
 
-  return <Nav />; // Render Nav otherwise
+  return <Nav />;
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-
-
+  const [showButton, setShowButton] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false)
   return (
     <Router>
-    
+      {showOverlay ?
+    <div className='matrix_overlay' id="matrix">
+      <div className='matrix_container'>
+        {!showButton && showOverlay ?
+        <div>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('Wake up, Neo...')
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('The Matrix has you...')
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('Follow the white rabbit.')
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('Knock, knock, Neo.')
+              .pauseFor(1500)
+              .deleteAll()
+              .callFunction(() => {
+                const audio = new Audio(doorKnock)
+                audio.play();
+                setShowButton(true)
+              })
+              .start();
+          }}
+          
+      />
+      </div>
+      : (
+
+        <button 
+        onClick={()=>{
+          setShowOverlay(false)
+        }}>
+          🐇<br></br>
+        </button>
+ 
+      )}
+      </div>
+    </div>
+    :
+    null}
     <ConditionalNav /> {/* Use ConditionalNav here */}
       <div className='pd-hz pd-vt ht-100vh'>
         <Routes>
--- src/pages/javascript/Javascript.jsx
+++ src/pages/javascript/Javascript.jsx
@@ -6,17 +6,45 @@ const items = [
     type:'project',
     title: 'AntiTherapy',
     description: 'A Python-based command-line application for managing basketball team statistics, featuring data import, player balancing, and detailed statistical displays. This tool streamlines team organization by balancing player experience, displaying comprehensive team stats, and offering a user-friendly menu interface.',
-    github: 'https://github.com/brent-thomas/Basketball-Stats-Tool',
-    live_link: '/javascript/projects/antitherapy'
+    links: [
+      {
+        Github: 'https://github.com/brent-thomas/Anti-Therapy-Bot'
+      }
+    ]
   },
+  {
+    type:'project',
+    title: 'GWSS',
+    description: 'Developed a custom web solution for a client, featuring responsive design, dynamic form validation, and integrated email notifications, tailored for efficient bulk wheat straw sales and delivery.',
+    links: [
+      {
+        Github: 'https://github.com/brent-thomas/gwss',
+      },
+      {
+        View: 'https://georgiawheatstrawsales.com'
+      }
+    ]
+  },
+  {
+    type:'project',
+    title: 'Zap URLS',
+    description: 'Developed and launched ZapURLs.com, a free, ad-free URL shortening service designed for simplicity and ease of use. Focused on providing a streamlined, hassle-free experience for individuals and small businesses, without the need for registration or dealing with intrusive ads. Perfect for sharing clean, user-friendly links across various platforms.',
+    links: [
+      {
+        Github: 'https://github.com/brent-thomas/gwss',
+      },
+      {
+        View: 'https://georgiawheatstrawsales.com'
+      }
+    ]
+  }
  
 ]
 
 const Javascript = () => {
   return (
     <div>
-      
-      <Link target="_blank" to={items[0].live_link}>Link</Link>
+        <GridDisplay items={items}/>
     </div>
   )
 }
